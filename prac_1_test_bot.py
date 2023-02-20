b55fdfc3a48c9cb2252fbc2aae8dd951e2684af5
Reinhardt von Brandis <reinhardtvbm@gmail.com>
2023-02-20 15:28:07 +0200

    more code

--- prac_1_test_bot.py
+++ prac_1_test_bot.py
@@ -1,5 +1,3 @@
-import random
-
 class Move:
     def __init__(self, value):
         self.value = value
@@ -16,7 +14,8 @@ class Node:
         self.children = []
 
     def expand_children(self):
-        self.children = [Node(self.move.value + 'R'), Node(self.move.value + 'P'), Node(self.move.value + 'S')]
+        prev_value = self.move.value
+        self.children = [Node(prev_value + 'R'), Node(prev_value + 'P'), Node(prev_value + 'S')]
 
     def get_children(self):
         return self.children
@@ -37,8 +36,6 @@ class Node:
             for child_node in current_node.children:
                 if child_node not in visited:
                     queue.append(child_node)
-
-
 
 def construct_tree(depth):
     # create the root node
@@ -67,83 +64,119 @@ def construct_tree(depth):
     # return the root node for traversing
     return root_node
 
-# n = construct_tree(4)
-# n.bfs()
+class RPS:
+    valid_chars = ['R', 'P', 'S']
 
-SEQUENCE_MIN_LEN = 2
-SEQUENCE_MAX_LEN = 5
+    def __init__(self) -> None:
+        pass
 
-inp = ""
-
-while inp != 'E':
-    inp = input()
-
-    if inp == 'start':
-        print('starting...')
-        # create the start node with "", and expand the children, so we get ['R', 'P', 'S'] in the queue
-        start_node = Node("")
-        start_node.expand_children()
-        
-        queue = start_node.children
-        
-        # the set of already visited nodes
-        visited = set()
-        break_sequence = curr_sequence
-        curr_sequence = []
-
-        prev_input = ""
-        curr_input = ""
-
-        in_break = False
-
-
-    if len(curr_sequence) == 0:
-        # make the current node the queue's first element 
-        curr_node = queue.pop(0)
-        
-        # add the current node to the already visited nodes
-        visited.add(curr_node)
-        sequence = curr_node.move.value
-
-        # the current sequence to test is now the list of characters contained 
-        # by the Node's move field
-        curr_sequence = list(sequence + sequence)
-        test_seq_len = len(curr_sequence)
-
-        curr_node.expand_children()
-
-        for child_node in curr_node.children:
-            if child_node not in visited:
-                queue.append(child_node)
-
-    prev_input = curr_input
-    curr_input = inp
-
+    def rock(self):
+        return 'R'
     
+    def paper(self):
+        return 'P'
+    
+    def scissors(self):
+        return 'S'
+    
+    def counter(self, move):
+        if move == "R":
+            return "P"
+        elif move == "P":
+            return "S"
+        elif move == "S":
+            return "R"
+        else:
+            return "C"
 
-    if in_break == False:
-        output = curr_sequence.pop(0)
+class GameData:
+    def __init__(self):
+        self.visited = set()
+        self.dfs_queue = []
+        self.break_sequence = []
+        self.prev_enemy_move = ""
+        self.curr_enemy_move = ""
+        self.counter_move = ""
+        self.index = 0
 
-        if curr_input == prev_input:
-            print("found sequence!")
-            in_break = True
+    def reset(self):
+        self.visited = set()
+        self.dfs_queue = []
+        self.prev_enemy_move = ""
+        self.curr_enemy_move = ""
+    
+    def next_in_break_seq(self):
+        next_move = self.break_sequence[self.index % len(self.break_sequence)]
+        self.index = (self.index + 1) % len(self.break_sequence)
 
-            visited = set()
-            curr_sequence = []
-            break_sequence = []
-            
-            if curr_input == 'R':
-                exploit = 'P'
-            elif curr_input == 'P':
-                exploit = 'S'
-            else:
-                exploit = 'R'
+        return next_move
+
+def deep_copy(list):
+    copy = []
+
+    for element in list:
+        copy.append(element)
+
+    return copy
+
+# while True:
+# inp = input()
+
+if input == "":
+    sequence_found = False
+
+    # create the start node with "", and expand the children, so we get ['R', 'P', 'S'] inp the queue
+    start_node = Node("")
+    start_node.expand_children()
+    
+    game_data = GameData()
+    game_data.dfs_queue = start_node.children
+
+    rps = RPS()
+
+    curr_sequence = []
+    prev_sequence = []
+    curr_seq_copy = []
+    repeating = False
+
+if len(curr_sequence) == 0 and sequence_found == False:
+    # make the current node the queue's first element 
+    curr_node = game_data.dfs_queue.pop(0)
+    
+    # add the current node to the already visited nodes
+    game_data.visited.add(curr_node)
+    sequence = curr_node.move.value
+
+    # the current sequence to test is now the list of characters contained 
+    # by the Node's move field
+    prev_sequence = curr_sequence
+    curr_sequence = list(sequence)
+    curr_seq_copy = deep_copy(curr_sequence)
+
+    curr_node.expand_children()
+
+    for child_node in curr_node.children:
+        if child_node not in game_data.visited:
+            game_data.dfs_queue.append(child_node)
+
+game_data.prev_enemy_move = game_data.curr_enemy_move
+game_data.curr_enemy_move = input
+
+if sequence_found == False:
+    output = curr_sequence.pop(0)
+
+    if game_data.prev_enemy_move == game_data.curr_enemy_move and game_data.curr_enemy_move in ["R", "P", "S"]:
+        sequence_found = True
+        repeating = True
+        game_data.break_sequence = curr_seq_copy
+        game_data.counter_move = rps.counter(game_data.curr_enemy_move)
+else:
+    repeating = game_data.prev_enemy_move == game_data.curr_enemy_move and game_data.curr_enemy_move in ["R", "P", "S"]
+
+    if repeating:
+        output = game_data.counter_move
     else:
-        output = exploit
-        
-        if prev_input != curr_input:
-            print("stopped repeating...")
-            in_break = False
+        output = game_data.next_in_break_seq()
+        game_data.counter_move = rps.counter(game_data.curr_enemy_move)
 
-
-    print(inp + ' : ' + output) 
+    # print(output)
